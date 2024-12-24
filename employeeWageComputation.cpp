ebaf559fc00746603c8dd2134e534cbce8c62069
Ajay Dhiman <ajaydhiman09012002@gmail.com>
2024-12-24 11:23:45 +0530

    [Ajay] ADD: Check Employee is Present or Absent.

--- employeeWageComputation.cpp
+++ employeeWageComputation.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
+
+void employeeAttendance(){
+  srand(time(0));
+  
+  int randomNum = rand() % 2;
+  
+  if(randomNum == 0){
+    cout << "Employee is Absent" << endl;
+  }else{
+    cout << "Employee is Present" << endl;
+  }
+}
+
 int main() 
 {
     cout << "Welcome to Employee Wage Computation" << endl;
-    
+    employeeAttendance();
     return 0;
 }
