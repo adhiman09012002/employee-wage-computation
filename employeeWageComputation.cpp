6b9eb3450effd13c4b408122ad0914c54fd2a090
Ajay Dhiman <ajaydhiman09012002@gmail.com>
2024-12-24 11:39:40 +0530

    [Ajay] ADD: calculate Daily Employee Wage.

--- employeeWageComputation.cpp
+++ employeeWageComputation.cpp
@@ -4,21 +4,34 @@
 
 using namespace std;
 
-void employeeAttendance(){
-  srand(time(0));
-  
-  int randomNum = rand() % 2;
-  
-  if(randomNum == 0){
-    cout << "Employee is Absent" << endl;
-  }else{
-    cout << "Employee is Present" << endl;
-  }
+
+
+int calculateDailyWage(int wagePerHour, int fullDayHour){
+    return wagePerHour * fullDayHour;
+
 }
 
-int main() 
-{
+void employeeAttendance(int wagePerHour, int fullDayHour){
+    srand(time(0));
+
+    int randomNum = rand() % 2;
+
+    if(randomNum == 0){
+        cout << "Employee is Absent" << endl;
+        cout << "Daily Wage: 0" << endl;
+    }else{
+        cout << "Employee is Present" << endl;
+        int dailyWage = calculateDailyWage(wagePerHour, fullDayHour);
+        cout << "Daily Wage: " << dailyWage << endl;
+    }
+}
+
+
+int main(){
     cout << "Welcome to Employee Wage Computation" << endl;
-    employeeAttendance();
+    int wagePerHour = 20; 
+    int fullDayHour = 8;  
+
+    employeeAttendance(wagePerHour, fullDayHour);
     return 0;
 }
